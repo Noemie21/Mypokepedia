2d49d1d25525c6ad5290f58ee90dd3db44ee916a
Noémie <noemie.kpatenon@supinternet.fr>
2021-06-20 17:23:44 +0200

    store x router

--- mypokepedia/src/router/index.js
+++ mypokepedia/src/router/index.js
@@ -1,19 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import ListPokemon from './views/ListPokemon.vue';
+import MyPokemon from './views/MyPokemon.vue';
+import PokemonDetail from './views/PokemonDetail.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'list pokemon',
+    component: ListPokemon,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/my-pokemon',
+    name: 'My pokemon',
+    component: MyPokemon,
+  },
+  {
+    path: '/detail/:name',
+    name: 'pokemon detail',
+    component: PokemonDetail,
   },
 ];
 
--- mypokepedia/src/store/index.js
+++ mypokepedia/src/store/index.js
@@ -1,10 +1,93 @@
-import { createStore } from 'vuex';
-import pokemon from './modules/pokemon';
-import ability from './modules/ability';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { Pokedex } from 'pokeapi-js-wrapper';
 
-export default createStore({
-  modules: {
-    pokemons: pokemon,
-    abilities: ability,
+const P = new Pokedex();
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    listPokemons: [],
+    onwnedPokemon: [],
+    selectedPokemon: null,
+    searchQuery: '',
+    isLoading: false
   },
+  mutations: {
+    setPokemonsList: (state, list) => {
+      state.listPokemons = [...list];
+    },
+    setSearchQuery: (state, searchQuery) => {
+      state.searchQuery = searchQuery;
+    },
+    setSlectedPokemon: (state, pokemon) => {
+      state.selectedPokemon = pokemon;
+    },
+    addCatchedPokemon: (state, nickname) => {
+      const newOwned = { ...state.selectedPokemon };
+      newOwned.nickname = nickname;
+
+      state.onwnedPokemon = [newOwned, ...state.onwnedPokemon];
+      state.listPokemons = state.listPokemons.map(poke => {
+        if (poke.name === state.selectedPokemon.name) {
+          if (poke.owned) {
+            poke.owned++;
+          } else {
+            poke.owned = 1;
+          }
+        }
+        return poke;
+      });
+    },
+    onReleasePokemon: (state, payload) => {
+      state.onwnedPokemon = state.onwnedPokemon.filter(
+        poke => poke.nickname !== payload.nickname
+      );
+      state.listPokemons = state.listPokemons.map(poke => {
+        if (poke.name === payload.name) {
+          if (poke.owned) {
+            poke.owned--;
+          } else {
+            poke.owned = 0;
+          }
+        }
+        return poke;
+      });
+    },
+  },
+  getters: {
+    filteredPokemon: state => {
+      if (state.searchQuery) {
+        return state.listPokemons.filter(poke =>
+          poke.name.toLowerCase().includes(state.searchQuery.toLowerCase())
+        );
+      } else {
+        return state.listPokemons;
+      }
+    },
+  },
+  actions: {
+    getPokemonsList: async ({ commit, state }) => {
+      state.isLoading = true;
+      try {
+        const data = await P.getPokemonsList();
+        commit('setPokemonsList', data.results);
+      } catch (error) {
+        /*eslint no-console: ['error', { allow: ['warn', 'error'] }] */
+        console.error(error);
+      }
+      state.isLoading = false;
+    },
+    getPokemonByName: async ({ commit, state }, pokemonName) => {
+      state.isLoading = true;
+      try {
+        const data = await P.getPokemonByName(pokemonName);
+        commit('setSlectedPokemon', data);
+      } catch (error) {
+        /*eslint no-console: ['error', { allow: ['warn', 'error'] }] */
+        console.error(error);
+      }
+      state.isLoading = false;
+    }
+  }
 });
